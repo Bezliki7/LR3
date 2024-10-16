91c09f7fd79dc4093f41170e5f65921768a7a560
Abaidullin_pi330 <seki4calm@gmail.com>
2024-10-17 00:09:08 +0500

    🔥 remove: убраны статические свойства

--- ElementAbaidullin/index.py
+++ ElementAbaidullin/index.py
@@ -1,10 +1,4 @@
-noDataValue = -1
-
 class ElementAbaidullin():
-    name = ''
-    symbol = ''
-    number = noDataValue
-
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
@@ -13,7 +7,6 @@ class ElementAbaidullin():
     def dump(self):
         print(f'name: {self.name} \nsymbol: {self.symbol} \nnumber: {self.number}')
 
-        
 
 object = ElementAbaidullin('Ruslan', 'H', 1)
 object.dump()
