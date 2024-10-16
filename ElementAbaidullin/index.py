633da328f926828980abc4daa38926f1a027e471
Abaidullin_pi330 <seki4calm@gmail.com>
2024-10-17 00:11:38 +0500

    ✅ task: добавлена приватность для  атрибутов

--- ElementAbaidullin/index.py
+++ ElementAbaidullin/index.py
@@ -1,13 +1,17 @@
 class ElementAbaidullin():
     def __init__(self, name, symbol, number):
+        self._name = name
+        self._symbol = symbol
+        self._number = number
         self.name = name
         self.symbol = symbol
         self.number = number
 
     def dump(self):
-        print(f'name: {self.name} \nsymbol: {self.symbol} \nnumber: {self.number}')
-
+        print(f'name: {self._name} \nsymbol: {self._symbol} \nnumber: {self._number}')
 
 object = ElementAbaidullin('Ruslan', 'H', 1)
 object.dump()
+
+print(object.name)
 
