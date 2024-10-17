5be6452c46fffb7a58002d2ee0f0cadafaeeeab4
Abaidullin_pi330 <seki4calm@gmail.com>
2024-10-17 22:18:18 +0500

    ✨ feat: определение статического метода about() для вывода информации по команде разработки

--- app/index.py
+++ app/index.py
@@ -1,7 +1,14 @@
 NO_VALUE = -1
 
+developersInfo = '1st developer: Abaidullin Ruslan \n2nd developer: Abzalov Timur \n3rd developer: Baulybaev Dinar'
+
 class Shape:
     s = NO_VALUE
     h = NO_VALUE
 
-    
+    @staticmethod
+    def about():
+        print(developersInfo)
+
+
+Shape.about()
