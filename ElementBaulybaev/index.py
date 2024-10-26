64f3091f95a81e60705e9bfd5e2f2c9e8a53f7ad
Baulybaev_pi330 <bqcrystal75@gmail.com>
2024-10-26 18:52:53 +0300

    ✅ Добавление dump

--- ElementBaulybaev/index.py
+++ ElementBaulybaev/index.py
@@ -1,14 +1,14 @@
-noDataValue = -1
-
 class ElementBaulybaev():
-    name = ''
-    symbol = ''
-    number = noDataValue
 
     def __init__(self, name, symbol, number):
         self.name = name
         self.symbol = symbol
         self.number = number
+
+    def dump(self):
+        print(f'name: {self.name} \nsymbol: {self.symbol} \nnumber: {self.number}')
         
 
-object = ElementBaulybaev('Ruslan', 'H', 1)
+object = ElementBaulybaev('Dinar', 'H', 1)
+object.dump()
+
