396e7f057f720a452c4cfdeb363ccca410c9ad06
Baulybaev_pi330 <bqcrystal75@gmail.com>
2024-10-26 18:42:20 +0300

    Создание объекта

--- ElementBaulybaev/index.py
+++ ElementBaulybaev/index.py
@@ -1,4 +1,14 @@
+noDataValue = -1
+
 class ElementBaulybaev():
     name = ''
     symbol = ''
-    number = ''
+    number = noDataValue
+
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+        
+
+object = ElementBaulybaev('Ruslan', 'H', 1)
